da6a17a28a78e695c19e3d01c3aa57b152df28a3
adrowicaksono <adrowicaksono@gmail.com>
2018-04-15 17:02:25 +0700

    upload file sorting array

---
+++ sorting/sorting_number-min-to-max.js
@@ -0,0 +1,42 @@
+var arr = [1, 9, 2, 9, 8, 5, 4, 1];
+
+console.log(arr);
+
+function swap(arr) {
+	
+	//membuat pengecekan
+	var signal = true;
+	for (j = 0; j < arr.length; j++) {
+		if (arr[j] > arr[j + 1]) {
+			signal = false;
+			break;
+		}
+	}
+
+	//console.log(signal);
+	if (signal === true) {
+		return arr;
+	}
+
+	var panjang = arr.length - 1;
+  //buble sort
+	for (var i = 0; i < panjang; i++) {
+		var min = 0;
+		var max = 0;
+		//console.log('ini indeks i:' + arr[i]);
+		//console.log('ini indeks i+1 :' + arr[i + 1]);
+		if (arr[i + 1] < arr[i]) {
+			min += arr[i + 1];
+			max += arr[i];
+		} else {
+			min += arr[i];
+			max += arr[i + 1];
+		}
+		arr[i] = min;
+		arr[i + 1] = max;
+	}
+	return swap(arr);
+}
+
+console.log(swap(arr));
+
